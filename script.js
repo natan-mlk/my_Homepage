61d9b2d4096127ba8280b897be034bd9deb4d9a0
natan-mlk <adam.milko@gmail.com>
2017-02-15 23:29:36 +0100

    animations on portfolio hover

--- script.js
+++ script.js
@@ -11,5 +11,34 @@ function colorMyBox() {
   }
   document.getElementById(randomNumber).style.fill = "#56a3b1";
 }
-
 window.setInterval(colorMyBox, 1600);
+
+// HOVER EFFECT FOR PORTFOLIO LINKS //
+//circle 4
+var isProject = document.getElementsByClassName("is_project")[0];
+isProject.addEventListener("mouseover", isProjectHov);
+isProject.addEventListener("mouseout", isProjectOut);
+
+function isProjectHov() {
+  var circle4 = document.getElementById("circle4");
+  circle4.classList.add('circle-hover');
+}
+
+function isProjectOut() {
+  var circle4 = document.getElementById("circle4");
+  circle4.className = 'circle circle4';
+}
+// circle 5
+var album = document.getElementsByClassName("album")[0];
+album.addEventListener("mouseover", albumHov);
+album.addEventListener("mouseout", albumOut);
+
+function albumHov() {
+  var circle4 = document.getElementById("circle5");
+  circle4.classList.add('circle-hover');
+}
+
+function albumOut() {
+  var circle4 = document.getElementById("circle5");
+  circle4.className = 'circle circle5';
+}
