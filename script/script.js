a626d6c08954a3c2911639ced9250d95b3a3e45b
natan-mlk <adam.milko@gmail.com>
2017-02-18 19:42:27 +0100

    fixed issues between scrolling and nav list

--- script/script.js
+++ script/script.js
@@ -51,7 +51,7 @@ $.scrollify({
   sectionName : "section-name",
   interstitialSection : "",
   easing: "easeOutExpo",
-  scrollSpeed: 1800,
+  scrollSpeed: 1500,
   offset : 0,
   scrollbars: true,
   standardScrollElements: "",
@@ -64,6 +64,11 @@ $.scrollify({
   afterResize:function() {},
   afterRender:function() {}
 });
+
+// SIDE PANEL
+
+
+
 
 
 // ANIMATION ON APPEAR
@@ -128,3 +133,83 @@ $(window).on('scroll', function () {
   }
 });
 
+
+
+
+
+
+
+
+
+
+
+
+// SCROLLSPY
+
+// Cache selectors
+var lastId,
+  topMenu = $("#top-menu"),
+  topMenuHeight = topMenu.outerHeight()+15,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function(){
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+  });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({
+    scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+  // Get container scroll position
+  var fromTop = $(this).scrollTop()+topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function(){
+    if ($(this).offset().top < fromTop)
+      return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length-1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent().removeClass("active")
+      .end().filter("[href='#"+id+"']").parent().addClass("active");
+  }
+});
+
+
+
+
+//
+// $('#section1').on('scrollSpy:enter', function() {
+//   $('.li1').addClass(".li-color");
+// });
+//
+// $('#section1').on('scrollSpy:exit', function() {
+//   $('.li1').addClass("li-color2");
+// });
+//
+// $('#section1').scrollSpy();
+
+
+// or you could do this:
+// $.scrollSpy($('.tile'));
+// or this
+// $('.tile').each(function(i, element) {
+//      $.scrollSpy(element);
+// });
